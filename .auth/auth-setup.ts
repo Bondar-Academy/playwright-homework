0fd40ea3e8331ba36bd72b31bdbc1b661ccabc97
Artem Bondar <artem.bondar@gmail.com>
2023-11-22 11:46:30 -0600

    auth setup update

--- .auth/auth-setup.ts
+++ .auth/auth-setup.ts
@@ -1,9 +1,25 @@
 import { chromium, type FullConfig } from '@playwright/test';
-import user from './user.json'
 
+const fs = require('fs').promises;
+const filePath = '.auth/user.json';
 
 async function browserAuthorize(config: FullConfig){
     const { baseURL, storageState } = config.projects[0].use;
+   
+    let user: any;
+    try {
+        const data = await fs.readFile(filePath);
+        user = JSON.parse(data);
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.log('User auth file does not exist, creating new file');
+            user = { cookies: [], origins: [] };
+            await fs.writeFile(filePath, JSON.stringify(user, null, 2));
+        } else {
+            console.error('Error processing user.json file:', err);
+            throw err;
+        }
+    }
 
     const browser = await chromium.launch({ headless: true });
     const context = await browser.newContext();
