8a0094a04a865544e78df8e6a5292d9c00f6cd2a
Artem Bondar <artem.bondar@gmail.com>
2023-11-22 12:27:53 -0600

    cleanup

--- tests/homework.spec.ts
+++ tests/homework.spec.ts
@@ -4,6 +4,6 @@ test.beforeEach( async({page}) => {
   await page.goto('/')
 })
 
-test('Home page is opened and Welcome message is displayed', async ({ page, request }) => {
+test('Home page is opened and Welcome message is displayed', async ({page}) => {
   await expect(page.locator('.title')).toHaveText('Welcome to Petclinic')
 });
