ee8b3442c6ac53525546b2c9e7ce1f108a8ab234
Artem Bondar <artem.bondar@gmail.com>
2023-11-27 14:56:16 -0600

    timeouts update and viewport

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,16 +5,19 @@ require('dotenv').config();
 export default defineConfig({
   fullyParallel: false,
   retries: 0,
-  workers: undefined,
+  workers: 0,
   reporter: 'html',
   globalSetup: '.auth/auth-setup.ts',
+  timeout: 90000,
   use: {
     baseURL: 'https://petclinic.bondaracademy.com',
     trace: 'on-first-retry',
     storageState: '.auth/user.json',
     extraHTTPHeaders: {
       'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
-    }
+    },
+    actionTimeout: 10000,
+    viewport: {height: 1080, width: 1920}
   },
 
   projects: [
